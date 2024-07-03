dcfff25cf7d4128251865b4996f87bb3c36c2df5
Jhonny Quevedo <quevedojhonny@gmail.com>
2024-07-03 01:03:45 -0400

    commit 2

--- frontend/src/assets/config.js
+++ frontend/src/assets/config.js
@@ -1,3 +1,5 @@
 /* require("dotenv").config() */
-const endpoint = process.env.ENDPOINT
+/* const endpoint = process.env.ENDPOINT */
+
+const endpoint = 'https://proyecto-final-2b4y.onrender.com'
 export { endpoint }
