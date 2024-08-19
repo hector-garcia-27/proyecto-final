15acafdb007745ad2d21f6568f5c2df0421aee28
Hector Garcia <garcia27h@hotmail.com>
2024-08-18 20:17:07 -0400

    app en local

--- frontend/src/assets/config.js
+++ frontend/src/assets/config.js
@@ -1,4 +1,4 @@
-const endpoint = 'https://proyecto-final-2b4y.onrender.com'
+//const endpoint = 'https://proyecto-final-2b4y.onrender.com'
 
-//const endpoint = "http://localhost:3000"
+const endpoint = "http://localhost:3000"
 export { endpoint }
